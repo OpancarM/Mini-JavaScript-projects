e68dcee0c6c2942f08abde90961fd026148ffc7c
Mateo Opančar <opancarmateo@gmail.com>
2023-12-03 16:59:03 +0100

    Create script.js
    Javascript unfinished

---
+++ JS-calculator/script.js
@@ -0,0 +1,5 @@
+const keys = document.querySelectorAll('.key');
+const dis_input = document.querySelector('.dis .input');
+const dis_output = document.querySelector('.dis .output');
+
+let input = "";
