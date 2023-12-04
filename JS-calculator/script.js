73f0922240e1bc94f7f4a2e3137bd852070b8e78
Mateo Opančar <opancarmateo@gmail.com>
2023-12-04 17:24:31 +0100

    Update

--- JS-calculator/script.js
+++ JS-calculator/script.js
@@ -3,3 +3,47 @@ const dis_input = document.querySelector('.dis .input');
 const dis_output = document.querySelector('.dis .output');
 
 let input = "";
+
+for (let key of keys) {
+	const value = key.dataset.key;
+
+	key.addEventListener('click', () => {
+		if (value == "clear") {
+			input = "";
+			dis_input.innerHTML = "";
+			dis_output.innerHTML = "";
+		} else if (value == "backspace") {
+			input = input.slice(0, -1);
+			dis_input.innerHTML = CleanInput(input);
+		} else if (value == "=") {
+			let result = eval(PerpareInput(input));
+
+			dis_output.innerHTML = CleanOutput(result);
+		} else if (value == "brackets") {
+			if (
+				input.indexOf("(") == -1 || 
+				input.indexOf("(") != -1 && 
+				input.indexOf(")") != -1 && 
+				input.lastIndexOf("(") < input.lastIndexOf(")")
+			) {
+				input += "(";
+			} else if (
+				input.indexOf("(") != -1 && 
+				input.indexOf(")") == -1 || 
+				input.indexOf("(") != -1 &&
+				input.indexOf(")") != -1 &&
+				input.lastIndexOf("(") > input.lastIndexOf(")")
+			) {
+				input += ")";
+			}
+
+			dis_isplay_input.innerHTML = CleanInput(input);
+		} else {
+			if (ValidateInput(value)) {
+				input += value;
+				dis_isplay_input.innerHTML = CleanInput(input);
+			}
+		}
+	})
+}
+
