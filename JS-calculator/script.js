4a9d3078fac89cc60296ad249dfe251d484d2621
Mateo Opančar <93339980+OpancarM@users.noreply.github.com>
2024-05-08 16:02:43 +0200

    BasicCalc

--- JS-calculator/script.js
+++ JS-calculator/script.js
@@ -1,49 +1,20 @@
-const keys = document.querySelectorAll('.key');
-const dis_input = document.querySelector('.dis .input');
-const dis_output = document.querySelector('.dis .output');
-
-let input = "";
-
-for (let key of keys) {
-	const value = key.dataset.key;
-
-	key.addEventListener('click', () => {
-		if (value == "clear") {
-			input = "";
-			dis_input.innerHTML = "";
-			dis_output.innerHTML = "";
-		} else if (value == "backspace") {
-			input = input.slice(0, -1);
-			dis_input.innerHTML = CleanInput(input);
-		} else if (value == "=") {
-			let result = eval(PerpareInput(input));
-
-			dis_output.innerHTML = CleanOutput(result);
-		} else if (value == "brackets") {
-			if (
-				input.indexOf("(") == -1 || 
-				input.indexOf("(") != -1 && 
-				input.indexOf(")") != -1 && 
-				input.lastIndexOf("(") < input.lastIndexOf(")")
-			) {
-				input += "(";
-			} else if (
-				input.indexOf("(") != -1 && 
-				input.indexOf(")") == -1 || 
-				input.indexOf("(") != -1 &&
-				input.indexOf(")") != -1 &&
-				input.lastIndexOf("(") > input.lastIndexOf(")")
-			) {
-				input += ")";
-			}
-
-			dis_isplay_input.innerHTML = CleanInput(input);
-		} else {
-			if (ValidateInput(value)) {
-				input += value;
-				dis_isplay_input.innerHTML = CleanInput(input);
-			}
-		}
-	})
-}
+function calculate(number1,number2,operator){
+	if (!number1 || !number2){
+		return "Invalid number"
+	}
+	if (!['*','-','/','+'].includes(operator)){
+		return "Invalid operator"
+	}
+	switch(operator){
+	  	case "+":
+			return number1 + number2;
+	  	case "/":
+			return number1 * number2;
+	  	case "*":
+			return number1 * number2;
+	  	case "-":
+			return number1 - number2;
+	 }
+	}
+	console.log(calculate(5,6,"+"))
 
